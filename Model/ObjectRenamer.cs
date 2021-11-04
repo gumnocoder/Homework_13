5f065249f7b46e0549bcc6d565a6e537d7deafd7
gumnocoder <84528014+gumnocoder@users.noreply.github.com>
2021-11-04 22:40:58 +0300

    Класс отвечающйи за переименовывание обьектов

---
+++ Model/ObjectRenamer.cs
@@ -0,0 +1,19 @@
+﻿using Homework_13.Model.Interfaces;
+
+namespace Homework_13.Model
+{
+    class ObjectRenamer<T> where T : INamedObject
+    {
+        readonly T obj;
+        readonly string newName;
+
+        public ObjectRenamer(T obj, string newName)
+        {
+            this.obj = obj;
+            this.newName = newName;
+            ReName();
+        }
+
+        private void ReName() => obj.Name = newName;
+    }
+}
