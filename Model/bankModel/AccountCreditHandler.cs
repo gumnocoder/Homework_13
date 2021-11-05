3a2ee5611d1171ca76e502b3e2258d3c01bde70c
gumnocoder <84528014+gumnocoder@users.noreply.github.com>
2021-11-05 15:53:43 +0300

    AccountCreditHandler добавлены методы

--- Model/bankModel/AccountCreditHandler.cs
+++ Model/bankModel/AccountCreditHandler.cs
@@ -21,6 +21,33 @@ namespace Homework_13.Model.bankModel
             _client = client;
             _account = (T)client.ClientsCreditAccount;
         }
+
+        public void ExtendCreditAmount(long amount)
+        {
+            Executed = false;
+            if (_client.Reputation > 7)
+            {
+                _account.AccountAmount -= amount;
+                new ReputationDecreaser(_client, 2);
+                Execute();
+            }
+        }
+        public void PayOff()
+        {
+            Executed = false;
+            _account.AccountAmount += _account.AccountAmount;
+            _client.CreditIsActive = false;
+            _client.ClientsCreditAccount = null;
+            new ReputationIncreaser(_client, 2);
+            Execute();
+        }
+
+        public void Pay(long PayAmount)
+        {
+            Executed = false;
+            _account.AccountAmount += PayAmount;
+            Execute();
+        }
         public void Execute() =>Executed = true;
     }
 }
