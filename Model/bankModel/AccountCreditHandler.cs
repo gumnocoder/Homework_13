7424131dae07cb740766ccc69f61d3ffeade1f5d
gumnocoder <84528014+gumnocoder@users.noreply.github.com>
2021-11-05 15:42:10 +0300

    Класс предоставляющий возможность оперирования кредитом

---
+++ Model/bankModel/AccountCreditHandler.cs
@@ -0,0 +1,26 @@
+﻿using Homework_13.Model.Interfaces;
+
+namespace Homework_13.Model.bankModel
+{
+    class AccountCreditHandler<T> : ICommandAction 
+        where T : BankCreditAccount
+    {
+        private Client _client;
+        private T _account;
+        private bool _executed = false;
+
+
+        public bool Executed
+        {
+            get => _executed;
+            set => _executed = value;
+        }
+
+        public AccountCreditHandler(Client client)
+        {
+            _client = client;
+            _account = (T)client.ClientsCreditAccount;
+        }
+        public void Execute() =>Executed = true;
+    }
+}
