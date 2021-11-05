0ae023e95050aa8349651ce37111d5947a452102
gumnocoder <84528014+gumnocoder@users.noreply.github.com>
2021-11-06 00:59:41 +0300

    MoneySender Класс, реализующий контрвариантный интерфейс, выполняющий перевод со счета на счёт

---
+++ Model/bankModel/MoneySender.cs
@@ -0,0 +1,27 @@
+﻿using Homework_13.Model.bankModel.interfaces;
+
+namespace Homework_13.Model.bankModel
+{
+    class MoneySender<T> : IMoneySender<T> where T : BankAccount
+    {
+        private bool CanSendMoney(Client client)
+        {
+            if (!client.AccountFreezed) return true;
+            else return false;
+        }
+        public void SendMoney(Client clientSender, long sum, T sender, T reciever)
+        {
+            if (CanSendMoney(clientSender))
+            {
+                if (sender != null && reciever != null)
+                {
+                    if (sender.AccountAmount >= sum)
+                    {
+                        sender.AccountAmount -= sum;
+                        reciever.AccountAmount += sum;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Model/bankModel/interfaces/IMoneySender.cs
@@ -0,0 +1,7 @@
+﻿namespace Homework_13.Model.bankModel.interfaces
+{
+    interface IMoneySender<in T>
+    {
+        void SendMoney(Client clientSender, long sum, T sender, T reciever);
+    }
+}
