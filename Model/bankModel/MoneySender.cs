e579f557dd7818afc0fb0248d700e96823e13793
gumnocoder <84528014+gumnocoder@users.noreply.github.com>
2021-11-13 12:08:55 +0300

    # MoneySender проверки на возможность отправки средств вынесены в отдельный метод

--- Model/bankModel/MoneySender.cs
+++ Model/bankModel/MoneySender.cs
@@ -1,26 +1,41 @@
-﻿using Homework_13.Model.bankModel.interfaces;
+﻿using System.Diagnostics;
+using Homework_13.Model.bankModel.interfaces;
 
 namespace Homework_13.Model.bankModel
 {
     class MoneySender<T> : IMoneySender<T> where T : BankAccount
     {
-        private bool CanSendMoney(Client client)
+        private bool CanSendMoney(Client client, long sum, T sender, T reciever)
         {
-            if (!client.AccountsFreezed) return true;
-            else return false;
+            if (!client.AccountsFreezed)
+            {
+                if (sender != null)
+                {
+                    if (reciever != null) return true;
+                    else
+                    {
+                        Debug.WriteLine("У получателя не существует данного счета");
+                        return false;
+                    }
+                }
+                else 
+                { 
+                    Debug.WriteLine("У отправителя не существует данного счета");
+                    return false;
+                }
+            }
+            else
+            {
+                Debug.WriteLine($"Счета отправителя {client} заморожены");
+                return false;
+            }
         }
         public void SendMoney(Client clientSender, long sum, T sender, T reciever)
         {
-            if (CanSendMoney(clientSender))
+            if (CanSendMoney(clientSender, sum, sender, reciever))
             {
-                if (sender != null && reciever != null)
-                {
-                    if (sender.AccountAmount >= sum)
-                    {
-                        sender.AccountAmount -= sum;
-                        reciever.AccountAmount += sum;
-                    }
-                }
+                sender.RemoveMoneyFromAccount(sum);
+                reciever.AddMoneyToAccount(sum);
             }
         }
     }
