01fc9794808ebcedb2e8ff1a9530cd48f67f0fab
gumnocoder <84528014+gumnocoder@users.noreply.github.com>
2021-12-28 00:19:31 +0300

    комментарии

--- Service/UserDialogService.cs
+++ Service/UserDialogService.cs
@@ -6,20 +6,38 @@ using Homework_13.View;
 using Homework_13.View.UserControls;
 using Homework_13.View.Windows;
 using Homework_13.ViewModel;
-using static Homework_13.ViewModel.ClientEditingFormViewModel;
-using static Homework_13.View.UserControls.UserCreationForm;
-using System.Diagnostics;
 
 namespace Homework_13.Service
 {
+    /// <summary>
+    /// открывает запрашиваемые окна в режиме диалога
+    /// </summary>
     class UserDialogService : IUserDialogService, IInformationDialogService
     {
+        /// <summary>
+        /// родительское окно
+        /// </summary>
         private static Window _owner = 
             Application.Current.Windows.OfType<Window>().FirstOrDefault(w => w.IsActive);
 
+        /// <summary>
+        /// Диалоговое окно для подтверждения операции
+        /// </summary>
+        /// <param name="Message">Текст описывающий операцию</param>
+        /// <param name="Tittle">Заголовок</param>
+        /// <param name="Choice">Выбор</param>
+        /// <returns></returns>
         public bool Confirm(string Message, string Tittle, bool Choice = false) =>
             MessageBox.Show(Message, Tittle, MessageBoxButton.YesNo) == MessageBoxResult.Yes;
 
+        /// <summary>
+        /// Запускает методы для выбранного класса,
+        /// индивидуально для Client или User
+        /// в противном случае запускает метод содержащий инструкции 
+        /// для других классов в качестве параметра
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
         public bool Edit(object o)
         {
             switch (o)
@@ -34,11 +52,52 @@ namespace Homework_13.Service
             return true;
         }
 
+        /// <summary>
+        /// метод содержащий инструкции которые выполняются 
+        /// в случае если в качестве параметра к методу edit 
+        /// были переданы не классы User или Client
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        private static bool GenericWindowOpenerMethod(object o)
+        {
+            var dlg = new Window();
+
+            switch (o)
+            {
+                /// создание пользователя
+                case UserCreationFormViewModel:
+                    dlg = new UserCreationForm();
+                    break;
+                /// создание клиета
+                case ClientCreationFormViewModel:
+                    dlg = new ClientCreationForm();
+                    break;
+                /// вывод списка пользователей
+                case UserListViewModel:
+                    dlg = new UserListView();
+                    break;
+                /// создание вывод списка клиентов
+                case ClientListViewModel:
+                    dlg = new ClientListView();
+                    break;
+            }
+
+            dlg.Owner = _owner;
+            dlg.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            if (dlg.ShowDialog() != true) return false;
+            return true;
+        }
+
+        /// <summary>
+        /// зарускает окно редактирования пользователя
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
         public bool EditUser(User user)
         {
-            Debug.WriteLine("1");
             UserEditingForm dlg = new();
-            Debug.WriteLine("2");
             dlg.DataContext = new UserEditingFormViewModel(user);
             dlg.Owner = _owner;
             dlg.WindowStartupLocation = WindowStartupLocation.CenterScreen;
@@ -47,21 +106,12 @@ namespace Homework_13.Service
 
             return true;
         }
-        public void ShowError(string Message)
-        {
-            MessageBox.Show(
-                Message, 
-                "Ошибка!",
-                MessageBoxButton.OK);
-        }
-        public void ShowInformation(string Message, string Tittle)
-        {
-            MessageBox.Show(
-                Message,
-                Tittle,
-                MessageBoxButton.OK);
-        }
 
+        /// <summary>
+        /// запускает окно редактирования клиента
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
         private static bool EditSelectedClient(Client client)
         {
             ClientEditingForm dlg = new();
@@ -73,41 +123,40 @@ namespace Homework_13.Service
 
             return true;
         }
+        
+        /// <summary>
+        /// вывод сообщение об ошибке
+        /// </summary>
+        /// <param name="Message">текст</param>
+        public void ShowError(string Message)
+        {
+            MessageBox.Show(
+                Message, 
+                "Ошибка!",
+                MessageBoxButton.OK);
+        }
 
+        /// <summary>
+        /// показывает оповещение
+        /// </summary>
+        /// <param name="Message">текст</param>
+        /// <param name="Tittle">заголовок</param>
+        public void ShowInformation(string Message, string Tittle)
+        {
+            MessageBox.Show(
+                Message,
+                Tittle,
+                MessageBoxButton.OK);
+        }
+
+        /// <summary>
+        /// открывает форму авторизации
+        /// </summary>
+        /// <param name="u"></param>
+        /// <returns></returns>
         private static bool LoginFormOpen(User u)
         {
             var dlg = new LoginFormWindow();
-
-            dlg.Owner = _owner;
-            dlg.WindowStartupLocation = WindowStartupLocation.CenterScreen;
-
-            if (dlg.ShowDialog() != true) return false;
-            return true;
-
-        }
-        private static bool GenericWindowOpenerMethod(object o)
-        {
-            var dlg = new Window();
-
-            switch (o)
-            {
-
-                case UserCreationFormViewModel:
-                    dlg = new UserCreationForm();
-                    break;
-
-                case ClientCreationFormViewModel:
-                    dlg = new ClientCreationForm();
-                    break;
-
-                case UserListViewModel:
-                    dlg = new UserListView();
-                    break;
-
-                case ClientListViewModel:
-                    dlg = new ClientListView();
-                    break;
-            }
 
             dlg.Owner = _owner;
             dlg.WindowStartupLocation = WindowStartupLocation.CenterScreen;
