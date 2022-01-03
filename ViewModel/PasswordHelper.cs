0fd1656e841612596b40ebb14b46fc3f96475007
gumnocoder <84528014+gumnocoder@users.noreply.github.com>
2022-01-04 00:25:10 +0300

    #

--- ViewModel/PasswordHelper.cs
+++ ViewModel/PasswordHelper.cs
@@ -3,44 +3,74 @@ using System.Windows.Controls;
 
 namespace Homework_13.ViewModel
 {
+    /// <summary>
+    /// 
+    /// </summary>
     class PasswordHelper
     {
-        public static readonly
-    DependencyProperty PasswordProperty =
-    DependencyProperty.RegisterAttached("Password",
-        typeof(string),
-        typeof(PasswordHelper),
-        new FrameworkPropertyMetadata(string.Empty, OnPasswordPropertyChanged));
+        /// <summary>
+        /// Статический конструктор
+        /// </summary>
+        static PasswordHelper()
+        {
+            PasswordProperty =
+                DependencyProperty.RegisterAttached(
+                    "Password",
+                    typeof(string),
+                    typeof(PasswordHelper),
+                    new FrameworkPropertyMetadata("", OnPasswordPropertyChanged));
 
-        public static readonly
-            DependencyProperty IsUpdatingProperty =
-            DependencyProperty.RegisterAttached("IsUpdating",
-                typeof(bool),
-                typeof(PasswordHelper));
+            IsUpdatingProperty =
+                DependencyProperty.RegisterAttached(
+                    "IsUpdating",
+                    typeof(bool),
+                    typeof(PasswordHelper));
 
-        public static readonly
-            DependencyProperty AttachProperty =
-            DependencyProperty.RegisterAttached("Attach",
-                typeof(bool),
-                typeof(PasswordHelper),
-                new PropertyMetadata(false, Attach));
+            AttachProperty =
+                DependencyProperty.RegisterAttached(
+                    "Attach",
+                    typeof(bool),
+                    typeof(PasswordHelper),
+                    new PropertyMetadata(false, Attach));
+        }
 
-        private static void OnPasswordPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        #region Поля
+
+        /// <summary>
+        /// Пароль
+        /// </summary>
+        public static readonly DependencyProperty PasswordProperty;
+        /// <summary>
+        /// Обновление пароля
+        /// </summary>
+        public static readonly DependencyProperty IsUpdatingProperty;
+        public static readonly DependencyProperty AttachProperty;
+
+        #endregion
+
+        #region Методы
+
+        private static void OnPasswordPropertyChanged(
+            DependencyObject d, 
+            DependencyPropertyChangedEventArgs e)
         {
             PasswordBox PassField = d as PasswordBox;
+
             PassField.PasswordChanged -= PasswordChanged;
-            if (!(bool)GetIsUpdating(PassField))
+
+            if (!GetIsUpdating(PassField))
             {
-                PassField.Password = (e.NewValue).ToString();
+                PassField.Password = e.NewValue.ToString();
             }
+
             PassField.PasswordChanged += PasswordChanged;
         }
 
-        public static bool GetIsUpdating(DependencyObject d)
-        {
-            return (bool)d.GetValue(IsUpdatingProperty);
-        }
-
+        /// <summary>
+        /// Уведомляет об изменении текста в PasswordBox и назначает этот текст свойству Password
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public static void PasswordChanged(object sender, RoutedEventArgs e)
         {
             PasswordBox PassField = sender as PasswordBox;
@@ -49,15 +79,31 @@ namespace Homework_13.ViewModel
             SetIsUpdating(PassField, false);
         }
 
+        /// <summary>
+        /// Сеттер для свойства Password
+        /// </summary>
+        /// <param name="passwordBox"></param>
+        /// <param name="password"></param>
         public static void SetPassword(PasswordBox passwordBox, string password)
         {
             passwordBox.SetValue(PasswordProperty, password);
         }
 
-        public static void SetIsUpdating(PasswordBox passwordBox, bool v)
-        {
+        /// <summary>
+        /// Сеттер для свойства IsUpdatingProperty, уведомляет об обновлении объекта
+        /// </summary>
+        /// <param name="passwordBox"></param>
+        /// <param name="v"></param>
+        public static void SetIsUpdating(PasswordBox passwordBox, bool v) =>
             passwordBox.SetValue(IsUpdatingProperty, v);
-        }
+
+        /// <summary>
+        /// Геттер для свойства IsUpdatingProperty, проверяет обновляется ли объект
+        /// </summary>
+        /// <param name="d"></param>
+        /// <returns></returns>
+        public static bool GetIsUpdating(DependencyObject d) => 
+            (bool)d.GetValue(IsUpdatingProperty);
 
         public static void Attach(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
@@ -77,14 +123,21 @@ namespace Homework_13.ViewModel
             }
         }
 
-        public static void SetAttach(DependencyObject d, bool v)
-        {
+        /// <summary>
+        /// Сеттер для свойства AtachProperty
+        /// </summary>
+        /// <param name="d"></param>
+        /// <param name="v"></param>
+        public static void SetAttach(DependencyObject d, bool v) => 
             d.SetValue(AttachProperty, v);
-        }
-
-        public static void GetAttach(DependencyObject d)
-        {
+        
+        /// <summary>
+        /// Геттер для свойства AtachProperty
+        /// </summary>
+        /// <param name="d"></param>
+        public static void GetAttach(DependencyObject d) => 
             d.GetValue(AttachProperty);
-        }
+
+        #endregion
     }
 }
